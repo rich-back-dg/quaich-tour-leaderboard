355dead91521512a371588454d1b258fc10c25e5
rich-back <backhouse.richard@gmail.com>
2024-05-06 12:31:50 +0100

    🔥 remove user greeting

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -20,7 +20,7 @@ export default async function AuthButton() {
 
   return user ? (
     <div className="flex items-center gap-4">
-      Hey, {user.email}!
+
       <form action={signOut}>
         <Button variant={"outline"} className="">
           Logout
