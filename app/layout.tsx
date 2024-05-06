e6bf301f1ec1dde9f9f255127399cc3dc76aa2ef
rich-back <backhouse.richard@gmail.com>
2024-05-06 12:21:00 +0100

    💄 add font setup to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import { GeistSans } from "geist/font/sans";
+import { Josefin_Sans as JoSans } from "next/font/google";
+import { Catamaran as Cata } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
 import { ThemeProvider } from "@/components/theme-provider";
@@ -13,26 +15,30 @@ export const metadata = {
   description: "The fastest way to build apps with Next.js and Supabase",
 };
 
+const cata = Cata({
+  subsets: ["latin"],
+  display: "swap"
+});
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning className={GeistSans.className}>
-      <body className="">
-      <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-        <main className="min-h-screen flex flex-col items-center">
+    <html lang="en" suppressHydrationWarning className={cata.className}>
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <main className="min-h-screen flex flex-col items-center">
             {children}
-            <Toaster position="top-right"/>
-        </main>
+            <Toaster position="top-right" />
+          </main>
         </ThemeProvider>
-
       </body>
     </html>
   );
