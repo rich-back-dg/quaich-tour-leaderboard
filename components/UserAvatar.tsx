0249b4d13b80095155bdcf24f5df49ef73fe87e3
rich-back <backhouse.richard@gmail.com>
2024-05-06 12:35:53 +0100

    ✨ create user avatar component

---
+++ components/UserAvatar.tsx
@@ -0,0 +1,33 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { createClient } from "@/utils/supabase/server";
+
+export default async function UserAvatar() {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  const { data: profile } = await supabase
+    .from("profiles")
+    .select("first_name, last_name")
+    .eq("id", user?.id)
+    .single();
+
+  const { data: avatar } = supabase.storage
+    .from("profiles")
+    .getPublicUrl(`${user?.id}/avatar`);
+
+  function createAvatarFallback() {
+    const fnameInitial = profile?.first_name.at(0);
+    const lnameInitial = profile?.last_name.at(0);
+    return fnameInitial + lnameInitial;
+  }
+
+  return (
+    <Avatar>
+      <AvatarImage src={avatar.publicUrl} alt="User Avatar Image"/>
+      <AvatarFallback>{createAvatarFallback()}</AvatarFallback>
+    </Avatar>
+  );
+}
