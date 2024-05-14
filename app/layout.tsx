73d4f3129b8eb9e4c50d7a3ba8b2b3ef99200eaa
rich-back <backhouse.richard@gmail.com>
2024-05-14 22:33:37 +0100

    💄 add new font and footer to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
-import { GeistSans } from "geist/font/sans";
-import { Josefin_Sans as JoSans } from "next/font/google";
 import { Catamaran as Cata } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Copyright } from "lucide-react";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -17,7 +16,7 @@ export const metadata = {
 
 const cata = Cata({
   subsets: ["latin"],
-  display: "swap"
+  display: "swap",
 });
 
 export default function RootLayout({
@@ -36,6 +35,11 @@ export default function RootLayout({
         >
           <main className="min-h-screen flex flex-col items-center">
             {children}
+            <footer className="w-full border-t p-6 flex justify-center text-center text-xs text-zinc-500">
+              <div className="flex gap-1 items-center">
+                <p>built by <u>richard backhouse</u>.</p>
+              </div>
+            </footer>
             <Toaster position="top-right" />
           </main>
         </ThemeProvider>
